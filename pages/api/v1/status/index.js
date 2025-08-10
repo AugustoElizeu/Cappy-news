26f1aa1cf3fc060743f4ab52d0efc7fed5f5e07f
Augusto Elizeu <augustocerrinho@hotmail.com>
2025-08-10 20:42:21 -0300

    finish */api/v1/status* endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
-import database from "infra/database";
+import database from "/infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionResult.rows[0].server_version.toString();
+
+  const dbMaxCon = await database.query("SHOW max_connections;");
+  const dbMaxConnection = dbMaxCon.rows[0].max_connections.toString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  console.log(databaseName);
+  const dbActiveCon = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const dbActiveConnections = dbActiveCon.rows[0].count;
+  console.log(dbActiveConnections);
+
   response.status(200).json({
-    chave: "Engel",
+    updated_at: updatedAt,
+    dependencies: {
+      db_version: dbVersion,
+      max_connection: parseInt(dbMaxConnection),
+      used_connection: dbActiveConnections,
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,14 @@
 test("GET to API/V1/STATUS should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log(response.status);
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.db_version).toBe("16.9");
+  expect(responseBody.dependencies.max_connection).toBe(100);
+  expect(responseBody.dependencies.used_connection).toBe(1);
+  console.log(parsedUpdatedAt);
 });
