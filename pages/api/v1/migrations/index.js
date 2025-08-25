a615bc3605706e54b741867e449df488c567efaf
Augusto Elizeu <augustocerrinho@hotmail.com>
2025-08-24 21:57:56 -0300

    endpoint  is working

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,8 +3,9 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
   const defaultMigrationOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
@@ -13,9 +14,9 @@ export default async function migrations(request, response) {
   };
 
   if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner({
-      defaultMigrationOptions,
-    });
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    console.log(pendingMigrations);
+    dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
   if (request.method === "POST") {
@@ -23,6 +24,7 @@ export default async function migrations(request, response) {
       ...defaultMigrationOptions,
       dryRun: false,
     });
+    await dbClient.end();
     if (migrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,7 +7,7 @@ test("GET to API/V1/STATUS should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.db_version).toBe("16.9");
+  expect(responseBody.dependencies.db_version).toBe("16.0");
   expect(responseBody.dependencies.max_connection).toBe(100);
   expect(responseBody.dependencies.used_connection).toBe(1);
   console.log(parsedUpdatedAt);
